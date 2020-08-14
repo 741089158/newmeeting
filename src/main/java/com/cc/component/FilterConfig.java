9357ee5fc0d15ec77b18a19d0d162ceb4af136d7
林文乐 <741089158@qq.com>
2020-08-14 11:15:01 +0800

    12

--- src/main/java/com/cc/component/FilterConfig.java
+++ src/main/java/com/cc/component/FilterConfig.java
@@ -1,30 +1,30 @@
-package com.cc.component;
-
-import javax.servlet.Filter;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import edu.yale.its.tp.cas.client.filter.CASFilter;
-
-@Configuration
-public class FilterConfig {
-
-	//单点登陆
-	//@Bean
-	public FilterRegistrationBean<Filter> testFilterRegistration() {
-		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
-		registration.setFilter(new CASFilter());
-		registration.addUrlPatterns("/*");
-		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.loginUrl", "http://ssoservice.ymtc.com/sso-server/login");
-		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.validateUrl", "http://ssoservice.ymtc.com/sso-server/serviceValidate");
-		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.serverName", "10.18.88.26:8080");
-		//registration.addInitParameter("edu.yale.its.tp.cas.client.filter.serverName", "localhost:8080");
-		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.initContextClass", "com.cc.component.MyContextInit");
-		registration.setName("SSO Filter");
-		registration.setOrder(1);
-		return registration;
-	}
-
-}
+//package com.cc.component;
+//
+//import javax.servlet.Filter;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import edu.yale.its.tp.cas.client.filter.CASFilter;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//	//单点登陆
+//	//@Bean
+//	public FilterRegistrationBean<Filter> testFilterRegistration() {
+//		FilterRegistrationBean<Filter> registration = new FilterRegistrationBean<>();
+//		registration.setFilter(new CASFilter());
+//		registration.addUrlPatterns("/*");
+//		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.loginUrl", "http://ssoservice.ymtc.com/sso-server/login");
+//		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.validateUrl", "http://ssoservice.ymtc.com/sso-server/serviceValidate");
+//		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.serverName", "10.18.88.26:8080");
+//		//registration.addInitParameter("edu.yale.its.tp.cas.client.filter.serverName", "localhost:8080");
+//		registration.addInitParameter("edu.yale.its.tp.cas.client.filter.initContextClass", "com.cc.component.MyContextInit");
+//		registration.setName("SSO Filter");
+//		registration.setOrder(1);
+//		return registration;
+//	}
+//
+//}
