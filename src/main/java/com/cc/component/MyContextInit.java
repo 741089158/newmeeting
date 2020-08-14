f65adcb0fed0f0b9aa7b6402b3c13d8ba97d1e76
林文乐 <741089158@qq.com>
2020-08-14 11:05:44 +0800

    12

--- src/main/java/com/cc/component/MyContextInit.java
+++ src/main/java/com/cc/component/MyContextInit.java
@@ -1,22 +1,22 @@
-package com.cc.component;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-
-import edu.yale.its.tp.cas.client.IContextInit;
-
-public class MyContextInit implements IContextInit {
-
-	@Override
-	public String getTranslatorUser(String userName) {
-		return userName;
-	}
-
-	@Override
-	public void initContext(ServletRequest request, ServletResponse servletResponse, FilterChain filterChain, String userName) {
-		((HttpServletRequest) request).getSession().setAttribute("username", userName);
-		//((HttpServletRequest) request).getSession().setAttribute("username", "A905663");
-	}
-}
+//package com.cc.component;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.http.HttpServletRequest;
+//
+//import edu.yale.its.tp.cas.client.IContextInit;
+//
+//public class MyContextInit implements IContextInit {
+//
+//	@Override
+//	public String getTranslatorUser(String userName) {
+//		return userName;
+//	}
+//
+//	@Override
+//	public void initContext(ServletRequest request, ServletResponse servletResponse, FilterChain filterChain, String userName) {
+//		((HttpServletRequest) request).getSession().setAttribute("username", userName);
+//		//((HttpServletRequest) request).getSession().setAttribute("username", "A905663");
+//	}
+//}
